7e4a16d047350b9696f212147fa15fee3bab5805
Swayam Aggarwal <168979100+Swayamagg@users.noreply.github.com>
2026-03-18 12:08:48 +0530

    Update cookie security settings in user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,8 +16,8 @@ export const authUser= async(req,res)=>{
         const token=await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameStrict:"strict",
+            secure:true,
+            sameStrict:"none",
             maxAge:7*24*60*60*1000
         })
         return res.status(200).json(user);
@@ -29,8 +29,8 @@ export const logOut=async(req,res)=>{
     try {
          res.clearCookie("token",{
             httpOnly:true,
-            secure:false,
-            sameStrict:"strict"
+            secure:true,
+            sameStrict:"none"
         });
         return res.status(200).json({message:"Logout successfully"});
     } catch (error) {
